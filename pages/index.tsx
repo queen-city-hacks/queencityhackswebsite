581783a148c0fb7197a12549a60ec63cff085dc5
citricacid2 <61765149+citricacid2@users.noreply.github.com>
2022-08-22 16:54:21 -0400

    better background and typing animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,19 @@ import styles from "../styles/Home.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
+import Typed from "react-typed";
+import "react-typed/dist/animatedCursor.css";
 
 const Home: NextPage = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   return (
     <>
-      <section className="h-screen bg-base text-white flex flex-col text-center">
+      <section
+        className="h-screen text-white flex flex-col text-center bg-[url('/dimgamers.jpg')] bg-center bg-cover backdrop-brightness-50"
+        id="header"
+      >
         {/* navbar */}
-        <div className="flex justify-between items-center h-16 p-10 py-16">
+        <div className="flex justify-between items-center h-16 p-10 py-16 bg-base">
           <div className="flex items-center">
             <Image
               src="/logo.png"
@@ -73,10 +78,14 @@ const Home: NextPage = () => {
         <div className="lg:h-1/5 h-1/6" />
         <div className="">
           <h1 className="font-special break-words text-xl hidden lg:block lg:text-6xl">
-            queen_city_hacks
+            <Typed strings={["queen_city_hacks"]} typeSpeed={80} />
           </h1>
           <h1 className="font-special break-words lg:hidden text-6xl">
-            queen city hacks
+            <Typed
+              strings={["queen city hacks"]}
+              typeSpeed={100}
+              showCursor={false}
+            />
           </h1>
           <h2 className="lg:text-4xl text-2xl pt-10 font-bold w-1/2 mx-auto">
             Charlotte’s biggest 12-hour, in-person, high-school hackathon!
