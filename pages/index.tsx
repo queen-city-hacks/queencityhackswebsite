eb54372d7e1cc95452b1762b3f548a20843db000
citricacid2 <61765149+citricacid2@users.noreply.github.com>
2022-08-21 20:26:40 -0400

    made links work

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,38 +52,34 @@ const Home: NextPage = () => {
       <section className=" bg-baseLight flex flex-col text-white p-10">
         <h2 className="text-3xl font-bold ">Frequently Asked Questions</h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 py-10">
-          <FAQ
-            question="What is Queen City Hacks?"
-            answer="Queen City Hacks is a 12-hour, in-person, high-school hackathon that
-        celebrates the creativity and innovation of Charlotte’s high school
-        students."
-          />
-          <FAQ
-            question="What is a hackathon?"
-            answer="A hackathon is a 24-hour, in-person, high-school hackathon that
-        celebrates the creativity and innovation of Charlotte’s high school
-        students."
-          />
-          <FAQ
-            question="How much does it cost to join?"
-            answer="This Hackathon is 100% free to join so all you have to do is sign up. 
-            Thanks to our sponsors we are able to host this event without any fees. 
-            We would greatly appreciate any Donations as it would improve the Hackathon greatly."
-          />
-          <FAQ
-            question="Who can participate?"
-            answer="Our primary patrons are mostly concentrated around the Greater Charlotte Area . 
-                  The majority of our patrons we advocate consist from grades 9-12, however, 
-                  if people grades 8 and under would like to participate then they may do so."
-          />
-          <FAQ
-            question="What are the prizes?"
-            answer="We will be giving away a variety of items at this event."
-          />
-          <FAQ
-            question="When is the event?"
-            answer="The dates are going to be between November and December of 2022."
-          />
+          <FAQ question="What is Queen City Hacks?">
+            Queen City Hacks is a 12-hour, in-person, high-school hackathon that
+            celebrates the creativity and innovation of Charlotte’s high school
+            students.
+          </FAQ>
+          <FAQ question="What is a hackathon?">
+            A hackathon is a 24-hour, in-person, high-school hackathon that
+            celebrates the creativity and innovation of Charlotte’s high school
+            students.
+          </FAQ>
+          <FAQ question="How much does it cost to join?">
+            This Hackathon is 100% free to join so all you have to do is sign
+            up. Thanks to our sponsors we are able to host this event without
+            any fees. We would greatly appreciate any Donations as it would
+            improve the Hackathon greatly.
+          </FAQ>
+          <FAQ question="Who can participate?">
+            Our primary patrons are mostly concentrated around the Greater
+            Charlotte Area . The majority of our patrons we advocate consist
+            from grades 9-12, however, if people grades 8 and under would like
+            to participate then they may do so.
+          </FAQ>
+          <FAQ question="What are the prizes?">
+            We will be giving away a variety of items at this event.
+          </FAQ>
+          <FAQ question="When is the event?">
+            The dates are going to be between November and December of 2022.
+          </FAQ>
         </div>
       </section>
     </>
@@ -94,14 +90,14 @@ export default Home;
 
 interface FAQProps {
   question: string;
-  answer: string;
+  children?: React.ReactNode;
 }
 
-const FAQ = ({ question, answer }: FAQProps) => {
+const FAQ = ({ question, children }: FAQProps) => {
   return (
     <div className="bg-base p-5 rounded-2xl shadow-2xl">
       <h2 className="text-2xl font-bold underline">{question}</h2>
-      <p className="text-lg pt-3">{answer}</p>
+      <p className="text-lg pt-3">{children}</p>
     </div>
   );
 };
