a38d059b47a25ce2496c0e7aa4fc610ee118393a
citricacid2 <61765149+citricacid2@users.noreply.github.com>
2022-08-23 16:41:09 -0400

    head section

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,31 @@ const Home: NextPage = () => {
   const [darkMode, setDarkMode] = useState("dark");
   return (
     <div className={darkMode}>
+      <Head>
+        <title>Home | queen_city_hacks</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+
       <section
         className="h-screen dark:text-white flex flex-col text-center bg-[url('/dimgamers.jpg')] bg-center bg-cover bg-fixed"
         id="header"
