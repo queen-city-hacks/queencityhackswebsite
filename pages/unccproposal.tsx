655f946bb4fcc169efa8d325dda2f43e824ac022
citricacid2 <61765149+citricacid2@users.noreply.github.com>
2022-09-24 16:23:43 -0400

    title

--- pages/unccproposal.tsx
+++ pages/unccproposal.tsx
@@ -3,6 +3,7 @@ import NotionPage from "../components/NotionPage";
 import { GetStaticProps } from "next";
 import { ExtendedRecordMap } from "notion-types";
 import Footer from "../components/Footer";
+import Head from "next/head";
 
 export const getStaticProps: GetStaticProps = async (context) => {
   return notionInfo("60d43be67ba64d5e9c602c2337aa85f0");
@@ -11,6 +12,9 @@ export const getStaticProps: GetStaticProps = async (context) => {
 export default function Team({ recordMap }: { recordMap: ExtendedRecordMap }) {
   return (
     <div className="relative pb-10 md:pb-0">
+      <Head>
+        <title>UNCC Proposal | queen_city_hacks</title>
+      </Head>
       <NotionPage recordMap={recordMap} />
       <Footer />
     </div>
