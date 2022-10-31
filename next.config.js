722f38ad7cdf7c3f56ae49738b52ac688dfc57f0
citricacid2 <61765149+citricacid2@users.noreply.github.com>
2022-10-31 17:25:40 -0400

    added redirects

--- next.config.js
+++ next.config.js
@@ -14,6 +14,16 @@ const nextConfig = {
           "https://organize.mlh.io/participants/events/8723-queen_city_hacks",
         permanent: true,
       },
+      {
+        source: "/discord",
+        destination: "https://discord.gg/fsrnAZaKGM",
+        permanent: true,
+      },
+      {
+        source: "/donate",
+        destination: "https://bank.hackclub.com/donations/start/queencityhacks",
+        permanent: true,
+      },
     ];
   },
 };
