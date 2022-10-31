9cea453c1f525a98b28bdaadc96caac6fca1591e
citricacid2 <61765149+citricacid2@users.noreply.github.com>
2022-10-31 11:08:47 -0400

    add redirect link

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ const nextConfig = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  async redirects() {
+    return [
+      {
+        source: "/register",
+        destination:
+          "https://organize.mlh.io/participants/events/8723-queen_city_hacks",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
