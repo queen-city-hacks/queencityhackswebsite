ab060abc9f2f67edd5a698e9d73d2223e92ad11a
hyusap <paulayush@gmail.com>
2024-04-06 14:48:34 -0400

    add lalit

--- components/TeamSection.tsx
+++ components/TeamSection.tsx
@@ -1,6 +1,7 @@
 import ayush from "../public/team/ayushpolaroid.png";
 import pranav from "../public/team/pranavpolaroid.png";
 import aditya from "../public/team/adityapolaroid.png";
+import lalit from "../public/team/lalitpolaroid.png";
 import Image, { StaticImageData } from "next/image";
 import Title from "./Title";
 import Section from "./Section";
@@ -9,7 +10,7 @@ const TeamSection = () => {
   return (
     <Section id="team">
       <Title text="Meet the Team" />
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 py-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8 py-10">
         <div className="rotate-2 md:translate-x-16 md:translate-y-7 md:rotate-6 hover:rotate-0 hover:translate-x-0 hover:translate-y-0 hover:scale-110 transition-all">
           <TeamMember name="Aditya Mehta" image={aditya}>
             Organizer and developer
@@ -17,13 +18,18 @@ const TeamSection = () => {
         </div>
         <div className="hover:scale-110 transition-all z-10">
           <TeamMember name="Ayush Paul" image={ayush}>
-            Lead organizer, developer and designer.
+            Director, developer and designer.
           </TeamMember>
         </div>
 
-        <div className="-rotate-2 md:-rotate-12 md:-translate-x-16 md:translate-y-4 hover:rotate-0 hover:translate-x-0 hover:translate-y-0 hover:scale-110 transition-all">
+        <div className="-rotate-2 md:-rotate-12 md:-translate-x-8 md:translate-y-4 hover:rotate-0 hover:translate-x-0 hover:translate-y-0 hover:scale-110 transition-all">
           <TeamMember name="Pranav Mucharla" image={pranav}>
             Organizer and logistical lead
+          </TeamMember>
+        </div>
+        <div className="rotate-2 md:-translate-x-16 md:translate-y-7 md:rotate-6 hover:rotate-0 hover:translate-x-0 hover:translate-y-0 hover:scale-110 transition-all">
+          <TeamMember name="Lalit Julapalli" image={lalit}>
+            Organizer and future director
           </TeamMember>
         </div>
       </div>
